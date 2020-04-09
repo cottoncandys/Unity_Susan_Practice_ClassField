f6219c075c7578b28e86b9d5d75552b3dd25fa60
susanbnak <bnak.tw@gmail.com>
2020-04-09 15:06:28 +0800

    新增Unity_Susan_Practice_ClassField

---
+++ ClassField/Assets/Scenes/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //血量
+    [Header("血量"),Range(50,500)]
+    [Tooltip("怪物的血量")]
+    public int blood ;
+
+    //魔力
+    [Header("魔力"), Range(10, 100)]
+    [Tooltip("怪物的魔力")]
+    public int magic = 100 ;
+
+    //移動速度
+    [Header("能力值"),Range(1, 50.5f)]
+    public float movingSpeed = 10.5f ;
+
+    //攻擊力
+    [Range(20,200)]
+    public int attack = 50 ;
+
+    //防禦力
+    [Range(0,100)]
+    public int defense ;
+
+    //武器名稱
+    [Header("裝備")]
+    public string weapon ;
+    //裝備名稱
+    public string equipment =  "皮衣";
+
+    //是否帶有鑰匙
+    [Header("其他資料")]
+    [Tooltip("怪物是否帶有鑰匙")]
+    public bool key = false;
+
+    //是否掉落寶物
+    [Tooltip("怪物是否帶有寶物")]
+    public bool treasure = false ;
+}
---
+++ ClassField/Assets/Scenes/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("怪物1號")]
+    public Enemy monster1;
+
+    [Header("怪物2號")]
+    public Enemy monster2;
+
+    private void Start()
+    {
+        print("怪物1號的魔力:" + monster1.magic);
+        print("怪物二號的裝備名稱:" + monster2.equipment);
+
+
+        monster1.blood = 100;
+        monster2.blood = 300;
+        monster1.defense = 50;
+        monster1.weapon = "木棍";
+        monster2.weapon = "短刀";
+        monster1.key = true;
+        monster2.treasure = true;
+    }
+}
