8d750a10bb6df534a043153d675b0600d486771e
susanbnak <bnak.tw@gmail.com>
2020-05-14 11:55:51 +0800

    更新summary

--- ClassField/Assets/Scenes/Enemy.cs
+++ ClassField/Assets/Scenes/Enemy.cs
@@ -2,40 +2,56 @@
 
 public class Enemy : MonoBehaviour
 {
-    //血量
-    [Header("血量"),Range(50,500)]
-    [Tooltip("怪物的血量")]
+    /// <summary>
+    /// 血量
+    /// </summary>
+    [Header("血量"), Tooltip("怪物的血量"), Range(50,500)]
     public int blood ;
 
-    //魔力
-    [Header("魔力"), Range(10, 100)]
-    [Tooltip("怪物的魔力")]
+    /// <summary>
+    /// 魔力
+    /// </summary>
+    [Header("魔力"), Tooltip("怪物的魔力"), Range(10, 100)]
     public int magic = 100 ;
 
-    //移動速度
-    [Header("能力值"),Range(1, 50.5f)]
-    public float movingSpeed = 10.5f ;
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [Header("能力值"), Range(1, 50.5f)]
+    public float speed = 10.5f ;
 
-    //攻擊力
+    /// <summary>
+    /// 攻擊力
+    /// </summary>
     [Range(20,200)]
     public int attack = 50 ;
 
-    //防禦力
+    /// <summary>
+    /// 防禦力
+    /// </summary>
     [Range(0,100)]
     public int defense ;
 
-    //武器名稱
+    /// <summary>
+    /// 武器名稱
+    /// </summary>
     [Header("裝備")]
     public string weapon ;
-    //裝備名稱
+    
+    /// <summary>
+    /// 裝備名稱
+    /// </summary>
     public string equipment =  "皮衣";
 
-    //是否帶有鑰匙
-    [Header("其他資料")]
-    [Tooltip("怪物是否帶有鑰匙")]
+    /// <summary>
+    /// 是否帶有鑰匙
+    /// </summary>
+    [Header("其他資料"), Tooltip("怪物是否帶有鑰匙")]
     public bool key = false;
 
-    //是否掉落寶物
+    /// <summary>
+    /// 是否掉落寶物
+    /// </summary>
     [Tooltip("怪物是否帶有寶物")]
     public bool treasure = false ;
 }
